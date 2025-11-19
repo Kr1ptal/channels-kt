b84016901bce8c9f791ac9658be77c0385e332ad
ArtificialPB <matej@kriptal.io>
2025-11-19 02:01:44 +0100

    fix: small project typo

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ jreleaser {
 
     // Set project info for deployment
     project {
-        description.set("High-performance Kotlin multiplatform Channel abstraction for JVM and iOS.")
+        description.set("High-performance Kotlin Multiplatform Channel abstraction for JVM and iOS.")
         links {
             homepage.set("https://github.com/Kr1ptal/channels-kt")
         }
